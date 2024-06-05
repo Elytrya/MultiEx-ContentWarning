ef6cd2f09bb16691a1208ee8a7a61c7a15363cdd
Elytrya <150997697+Elytrya@users.noreply.github.com>
2024-06-05 09:57:02 +0300

    Add files via upload

---
+++ plugin.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+
+namespace MultiEx
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class MultiEx : BaseUnityPlugin
+    {
+        private const string modGUID = "MultiEx";
+        private const string modName = "MultiEx";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo("MultiEx успешно загружен и запущен.");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+        }
+    }
+}
