dab7c0dc71ecb0d2cc37c3d552823fc993b473e5
elytrya <150997697+Elytrya@users.noreply.github.com>
2025-03-25 16:07:49 +0300

    Update InfinityHeathPatch.cs

--- MultiExt/patches/InfinityHeathPatch.cs
+++ MultiExt/patches/InfinityHeathPatch.cs
@@ -8,11 +8,9 @@ namespace MultiEx.Patches
     {
         static MethodBase TargetMethod()
         {
-            // Попробуйте найти метод Update через рефлексию
             MethodBase method = AccessTools.Method(typeof(Player.PlayerData), "Update");
             if (method == null)
             {
-                // Если метод не найден, добавьте здесь дополнительную логику поиска
                 foreach (var m in typeof(Player.PlayerData).GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public))
                 {
                     if (m.Name.Contains("Update"))
@@ -29,11 +27,9 @@ namespace MultiEx.Patches
         [HarmonyPostfix]
         public static void UpdateInfinityHeath(Player.PlayerData __instance)
         {
-            // Проверяем, существует ли поле maxOxygen в Player.PlayerData
             var health = AccessTools.Field(typeof(Player.PlayerData), "health");
             if (health != null)
             {
-                //                // Устанавливаем значение health на 1000f
                 AccessTools.Field(typeof(Player.PlayerData), "health").SetValue(__instance, 1000f);
             }
         }
